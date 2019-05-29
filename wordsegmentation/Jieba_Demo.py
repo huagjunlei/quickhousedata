7bbf085e0fec048078ec7d32dedbdbb6dd867e5a
huangjunlei <junlei-huang@outlook.com>
2019-05-29 17:20:36 +0800

    add jieba url

--- wordsegmentation/Jieba_Demo.py
+++ wordsegmentation/Jieba_Demo.py
@@ -1,5 +1,5 @@
 # encoding=utf-8
-import jieba
+import jieba #https://github.com/fxsjy/jieba
 sentence = '中文分词是文本处理不可或缺的一步！'
 seg_list = jieba.cut(sentence, cut_all=True)
 print('全模式：','/'.join(seg_list)) #全模式： 中文/分词/是/文本/文本处理/本处/处理/不可/不可或缺/或缺/的/一步//
